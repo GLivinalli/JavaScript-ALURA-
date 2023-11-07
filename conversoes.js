fdae24e3cf6486583fe249a69a90749e59d4653a
GLivinalli <gabriellivinalli1@hotmail.com>
2023-11-07 19:53:52 -0400

    add

---
+++ conversoes.js
@@ -0,0 +1,15 @@
+// tipo de dado
+// booleanos 
+
+// conversão implícita
+
+// conversão explícita 
+
+const numero1 = 123;
+const numero2 = "123";
+
+// Number()
+console.log(numero1 + Number(numero2)) 
+
+// String()
+console.log(String(numero1) + numero2)
