6ca5d6278596d1be236ed9fc0138a94cdd491e03
GLivinalli <gabriellivinalli1@hotmail.com>
2023-11-07 19:35:07 -0400

    ADD

---
+++ true-false.js
@@ -0,0 +1,22 @@
+// boolean 
+
+const usuarioLogado = true
+const contaPaga = false
+
+// truthy or falsy
+
+// 0 > false
+// 1 > true
+
+console.log(0 == false)
+console.log("" == false)
+console.log(1 == true)
+
+
+// null
+// undefined 
+
+let valorUndefined;
+const valorNull = null;
+
+console.log(valorUndefined, valorNull)
