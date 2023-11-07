bdd7599515fef46e498019800e14ca5c19b5ed42
GLivinalli <gabriellivinalli1@hotmail.com>
2023-11-07 09:01:07 -0400

    first commit

---
+++ JS.js
@@ -0,0 +1,42 @@
+// Numero
+
+const meuNumero = 3;
+
+const primeiroNum = 1;
+const segundoNum = 4;
+
+const mathop = primeiroNum + segundoNum;
+
+console.log("Numeber: ", mathop)
+
+// ponto Flutuante
+
+const primeiroPontoF = 3.3;
+const segundoPontoF = .5;
+
+const novaOP = primeiroPontoF / segundoPontoF;
+
+console.log("Ponto Flutuante: ", novaOP)
+
+// String
+
+const texto1 = "Hello world";
+const texto2 = "José falou 'OIE'";
+const stringDeNumero = "123";
+
+console.log("String: ",texto1 + texto2 + stringDeNumero)
+
+// concatenação (+)
+
+const citacao = "Meu nome é ";
+const meuNome = "Gabriel";
+
+console.log("Concatenação: " + citacao + meuNome)
+
+//Boolean
+
+const num1 = 5;
+const num2 = 10;
+
+console.log("Valor Booleano", num1 === num2)
+
