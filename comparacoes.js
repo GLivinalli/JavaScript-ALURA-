dbc74bf9d2f925993eb7345a4988e04a7ed0ceb2
GLivinalli <gabriellivinalli1@hotmail.com>
2023-11-08 11:53:00 -0400

    ADD

---
+++ comparacoes.js
@@ -0,0 +1,22 @@
+// == comparação implícita
+
+ const numero = 5;
+ const texto = "5";
+ 
+ console.log(numero == texto)
+
+
+//typeof mostra o tipo de dado
+
+ console.log(typeof numero)
+
+// == compara o valor
+// === compara o tipo de dado e o valor (boa pratica)
+// usar os metodos de conversão Number() String()
+
+// ||: Operador “ou”, retorna true caso uma condição seja válida;
+// &&: Operador “e”, retorna true somente se todas as condições forem válidas;
+// != e !==: Operadores “não igual” e “estritamente não igual”, utilizados para comparação, 
+//  da mesma forma que == e === retornam true ou false.
+
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Expressions_and_operators
